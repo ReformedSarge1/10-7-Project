1672be822bf05ac9935e13e7062e19c592151bc7
Carter Cook <raymanomen3014@gmail.com>
2022-10-11 22:44:02 -0700

    Power Went out and lost most of the work

---
+++ Rental-Project.py
@@ -0,0 +1,22 @@
+
+import random
+
+
+class rental():
+    def __init__(self):
+        self.income = 16.0
+        self.cash = 1500
+        self.investment = 3000
+        self.entire_investment = 24000
+
+
+    def income(self):
+        self.income = random * int
+    if income == 'y':
+            print("this is your income this year")                               
+            investment = round(random*int(70, 12.00), 2)
+            entire_investment = ("ROI")
+    ROI = (1500 * 16.0)
+    print(ROI)
+    
+   
